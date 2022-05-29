fa04d19e5576f8611acf368b9d2904f0d1d8462e
piter7464 <p.graczyk@hotmail.com>
2022-05-29 22:38:51 +0200

    d

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+purse = dict()
+
+purse['money'] = 12
+purse['candy'] = 3
+purse['keys'] = 75
+
+print(purse)
+
+print(purse['candy'])
+
+purse['candy'] = purse['candy'] + 2
+print(purse['candy'])
+print(purse)
