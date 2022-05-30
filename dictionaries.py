a4f0c52fcc2ee11c9e5ea21870799dae2e6277bd
piter7464 <p.graczyk@hotmail.com>
2022-05-30 14:35:03 +0200

    d

--- dictionaries.py
+++ dictionaries.py
@@ -33,3 +33,8 @@ print(counts)
 
 counts = { 'quincy' : 1 , 'mrugesh' : 42, 'beau': 100, '0': 10}
 print(counts.get('kris', 0))
+
+counts = { 'chuck' : 1 , 'annie' : 42, 'jan': 100}
+for key in counts:
+    if counts[key] > 10:
+        print(key, counts[key])
