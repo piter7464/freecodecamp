198c43f09d2bdff7f5af89b92c3ace4698050c9b
piter7464 <p.graczyk@hotmail.com>
2022-06-02 19:36:42 +0200

    d

--- lists.py
+++ lists.py
@@ -95,3 +95,28 @@ print(sorted(lista))
 lista = [5,3,7,2,1]
 print(sorted(lista, reverse=True))
 
+# for string
+seq_string = 'Python'
+print(list(reversed(seq_string)))
+
+# for tuple
+seq_tuple = ('P', 'y', 't', 'h', 'o', 'n')
+print(list(reversed(seq_tuple)))
+
+# for range
+seq_range = range(5, 9)
+print(list(reversed(seq_range)))
+
+# for list
+seq_list = [1, 2, 4, 3, 5]
+print(list(reversed(seq_list)))
+
+
+# create a list of prime numbers
+prime_numbers = [2, 3, 5, 7]
+
+# reverse the order of list elements
+prime_numbers.reverse()
+
+
+print('Reversed List:', prime_numbers)
--- strings.py
+++ strings.py
@@ -20,29 +20,3 @@ print(x)
 
 xy = 'HELLO'
 print(xy.lower())
-
-# for string
-seq_string = 'Python'
-print(list(reversed(seq_string)))
-
-# for tuple
-seq_tuple = ('P', 'y', 't', 'h', 'o', 'n')
-print(list(reversed(seq_tuple)))
-
-# for range
-seq_range = range(5, 9)
-print(list(reversed(seq_range)))
-
-# for list
-seq_list = [1, 2, 4, 3, 5]
-print(list(reversed(seq_list)))
-
-
-# create a list of prime numbers
-prime_numbers = [2, 3, 5, 7]
-
-# reverse the order of list elements
-prime_numbers.reverse()
-
-
-print('Reversed List:', prime_numbers)
