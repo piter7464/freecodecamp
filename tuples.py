c8dde52fd7b566f9f2e352764dad3f5d9e30f144
piter7464 <p.graczyk@hotmail.com>
2022-06-02 21:04:02 +0200

    d

---
+++ tuples.py
@@ -0,0 +1,16 @@
+(x, y) = (4, 'fred')
+
+print(y)
+
+d = dict()
+d['quincy'] = 1
+d['beau'] = 5
+d['kris'] = 9
+for (k,i) in d.items():
+    print(k, i)
+
+if (0, 1, 2) < (5, 1, 2): ## is comparing 0 to 5 and if it's lower then it's stoping comparing
+    print('True')
+
+if ('Jones', 'Sally') < ('Jones', 'Sam'): ## because L in Sally is less than M it's comparing alphabet
+    print('True')
